53934f0881e361e81dfc1ee613118c7b13a34506
Svenn Tveit <svtv@norceresearch.no>
2026-08-09 14:01:53 +0200

    Default --wells to on in opm-vis-pv

--- src/opm_vis/cli/pvplot_cli.py
+++ src/opm_vis/cli/pvplot_cli.py
@@ -102,9 +102,9 @@ def _wells_slice(
 )
 @click.option("--log-scale", is_flag=True, default=False, help="Map colours logarithmically.")
 @click.option(
-    "--wells",
-    is_flag=True,
-    default=False,
+    "--wells/--no-wells",
+    default=True,
+    show_default=True,
     help="Draw wells with a completion on the chosen slice.",
 )
 @click.option(
